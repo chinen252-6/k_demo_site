a41e9dfd166ada131d7b19ce04b599e211eb5de5
chinen252-6 <satoukibi6@gmail.com>
2024-07-16 10:49:04 +0900

    'コンタクトフォーム途中'

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,6 @@
 
-    $('.title').on('click', function() {//タイトル要素をクリックしたら
-  var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+    $('.question__accordion-area__flex').on('click', function() {//タイトル要素をクリックしたら
+  var findElm = $(this).next(".question__accordion-area__bottom");//直後のアコーディオンを行うエリアを取得し
   $(findElm).slideToggle();//アコーディオンの上下動作
     
   if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
@@ -14,9 +14,9 @@
 $(window).on('load', function(){
   $('.question__accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
   $(".open").each(function(index, element){ //openクラスを取得
-    var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
+    var Title =$(element).children('.question__accordion-area__flex'); //openクラスの子要素のtitleクラスを取得
     $(Title).addClass('close');       //タイトルにクラス名closeを付与し
-    var Box =$(element).children('.box'); //openクラスの子要素boxクラスを取得
+    var Box =$(element).children('.question__accordion-area__bottom'); //openクラスの子要素boxクラスを取得
     $(Box).slideDown(500);          //アコーディオンを開く
   });
 });
